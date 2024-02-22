ea0a819bfcd2504b2e6bb139a0469b1802fc9c6f
Clément Voisin <clembs@clembs.com>
2024-02-22 15:18:56 +0100

    feat: wip projects section

---
+++ src/routes/(home)/Projects.svelte
@@ -0,0 +1,25 @@
+<section id="projects">
+	<ul>
+		{#each new Array(25).fill('') as project}
+			<li />
+		{/each}
+	</ul>
+</section>
+
+<style lang="scss">
+	#projects ul {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(315px, 1fr));
+		gap: var(--space-md);
+		width: 90%;
+		max-width: 1200px;
+		margin: 0 auto;
+		padding: 3rem 0;
+
+		li {
+			background-color: var(--color-emphasis);
+			width: 100%;
+			aspect-ratio: 1.3 / 1;
+		}
+	}
+</style>
