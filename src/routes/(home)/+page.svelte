644d565aae4cf1f67cc9cfe2d72d2bcf6d84492e
Clément Voisin <clembs@clembs.com>
2024-02-22 15:19:25 +0100

    feat: home page

---
+++ src/routes/(home)/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import AboutMe from './AboutMe.svelte';
+	import Header from './Header.svelte';
+	import Projects from './Projects.svelte';
+</script>
+
+<main>
+	<Header />
+	<AboutMe />
+	<Projects />
+</main>
+
+<style lang="scss">
+	:global(.highlighted) {
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		background-image: var(--gradient-background);
+	}
+</style>
