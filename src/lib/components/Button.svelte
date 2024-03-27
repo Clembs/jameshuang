b6856791110f7caa9af5ee422bfb41e4a530a337
Clément Voisin <clembs@clembs.com>
2024-03-27 16:18:17 +0100

    feat: button and textinput components

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let type: 'submit' | 'button' = 'submit';
+	export let style: 'filled' | 'outline' = 'filled';
+</script>
+
+<button on:click {type} class={style}>
+	<slot />
+</button>
+
+<style lang="scss">
+	button {
+		padding: 0.5rem 1.5rem;
+		font: inherit;
+		font-size: var(--font-size-md);
+		border-radius: 0.25rem;
+		margin-top: 0.5rem;
+
+		&.filled {
+			background-color: var(--color-foreground-full);
+			color: var(--color-background);
+			border: 1px solid transparent;
+
+			&:hover {
+				background-color: var(--color-foreground-medium);
+			}
+		}
+
+		&.outline {
+			background-color: transparent;
+			color: var(--color-foreground-full);
+			border: 1px solid var(--color-foreground-full);
+
+			&:hover {
+				background-color: var(--color-foreground-lowest);
+			}
+		}
+	}
+</style>
---
+++ src/lib/components/TextInput.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let type: 'email' | 'password' | 'text' | 'number';
+	export let name: string;
+	export let placeholder: string = '';
+	export let value: string = '';
+	export let label: string = '';
+	export let required: boolean = true;
+	export let error = '';
+</script>
+
+<div class="text-input">
+	{#if label}
+		<label for={name}>
+			{label}
+		</label>
+	{/if}
+
+	<input {placeholder} {name} {type} {value} {required} />
+
+	{#if error}
+		<p class="error">{error}</p>
+	{/if}
+</div>
+
+<style lang="scss">
+	.text-input {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		font-size: var(--font-size-sm);
+
+		input {
+			padding: 0.5rem;
+			font: inherit;
+			font-size: var(--font-size-md);
+			border-radius: 0.25rem;
+			border: 1px solid var(--color-foreground-full);
+		}
+
+		.error {
+			color: #ff0000;
+		}
+	}
+</style>
