b5350763d15462c66b36ad5497798ce5385406e9
Clément Voisin <clembs@clembs.com>
2024-04-20 17:35:34 +0200

    feat: custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<main>
+	<h1>{$page.status}</h1>
+	{#if $page.status === 404}
+		<p>The page you were looking for was not found.</p>
+	{:else if $page.status >= 500 && $page.status < 600}
+		<p>An error happened on our end. Please try again later.</p>
+	{:else}
+		<p>An error happened. Please try again later.</p>
+	{/if}
+
+	<Button href="/">Go back to the home page</Button>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		text-align: center;
+		flex-direction: column;
+		gap: 1rem;
+
+		h1 {
+			font-size: var(--font-size-xxl);
+		}
+	}
+</style>
