68ba4ef3a621e2ad904d1437b01b5caeed462200
Clément Voisin <clembs@clembs.com>
2025-04-05 22:36:57 +0200

    fix: testimonials take too much width

--- src/routes/(home)/Testimonials.svelte
+++ src/routes/(home)/Testimonials.svelte
@@ -129,11 +129,15 @@
 	#testimonials-section {
 		padding: var(--space-xxl) var(--space-xxl);
 		margin: 0 auto;
+
+		@media (max-width: 600px) {
+			padding: var(--space-md) var(--space-sm);
+		}
 	}
 
 	#testimonials {
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(420px, 1fr));
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 		gap: var(--space-lg);
 		list-style: none;
 		padding: 0;
